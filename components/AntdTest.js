a8934e9fdd92badc8b943c1ad8fd1c60dc5d56ff
lin pascal <dunlintan@hk01.com>
2020-07-24 16:39:02 +0800

    feat: init files

---
+++ components/AntdTest.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, DatePicker, version } from "antd";
+
+export default {
+  title: "Antd-Test",
+  component: Button,
+};
+export function AntdTest() {
+  return (
+    <div className="App">
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </div>
+  );
+}
