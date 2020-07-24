cb029d3e68063e520af4008d5a5649336e9da66d
lin pascal <dunlintan@hk01.com>
2020-07-24 17:13:09 +0800

    feat: add graphql

---
+++ stories/Graphql.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/react";
+import { setupGraphiQL } from "@storybook/addon-graphql";
+
+// setup the graphiql helper which can be used with the add method later
+const graphiql = setupGraphiQL({ url: "http://localhost:3000/graphql" });
+
+storiesOf("GraphQL Demo", module).add(
+  "get user info",
+  graphiql(`{
+    user(id: "1") {
+      name
+    }
+  }`)
+);
